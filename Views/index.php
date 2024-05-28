5efb6bc45c8baf4be8ac948b8aa7211fc68f4efa
Kevin Andres Saldaña Acosta <kevin.saldaa@IMEVI.CO>
2024-05-28 11:23:02 -0500

    o

--- Views/index.php
+++ Views/index.php
@@ -1,5 +1,5 @@
 <!doctype html>
-<html lang="en">
+<html lang="es">
   <head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
@@ -11,18 +11,16 @@
     <title>Hello, world!</title>
   </head>
   <body>
-    <div class="containee">
-    <div class="calendar">
-    </div>
-    </div>
+    <div id='calendar'></div>
 
     <!-- Optional JavaScript; choose one of the two! -->
 
     <!-- Option 1: Bootstrap Bundle with Popper -->
-    <script src="<?php echo base_url;?>Assets/js/bootstrap.bundle.min.js"></script>
-    <script src="<?php echo base_url;?>Assets/js/index.global.min.js"></script>
-    <script src="<?php echo base_url;?>Assets/js/moment.js"></script>
-    <script src="<?php echo base_url;?>Assets/js/sweetalert2.all.min.js"></script>
-    <script src="<?php echo base_url;?>Assets/js/app.js"></script>
+    <script src="<?php echo base_url; ?>Assets/js/bootstrap.bundle.min.js"></script>
+    <script src="<?php echo base_url; ?>Assets/js/index.global.min.js"></script>
+    <script src="<?php echo base_url; ?>Assets/js/moment.js"></script>
+    <script src="<?php echo base_url; ?>Assets/js/sweetalert2.all.min.js"></script>
+    <script src="<?php echo base_url; ?>Assets/js/app.js"></script>
+    <script src="<?php echo base_url; ?>Assets/js/index.global.js"></script>
   </body>
 </html>
