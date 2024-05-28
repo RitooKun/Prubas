6721f020e1980b5e0608c2dfbc2c19b1a5eb4384
Kevin Andres Saldaña Acosta <kevin.saldaa@IMEVI.CO>
2024-05-28 16:01:13 -0500

    calendario

--- Assets/js/app.js
+++ Assets/js/app.js
@@ -1,3 +1,4 @@
+var myModal = new bootstrap.Modal(document.getElementById('mymodal'));
 document.addEventListener('DOMContentLoaded', function() {
     var calendarEl = document.getElementById('calendar');
     var calendar = new FullCalendar.Calendar(calendarEl, {
@@ -7,7 +8,13 @@ document.addEventListener('DOMContentLoaded', function() {
         left : 'prev, next, today',
         center: 'title',
         right:'dayGridMonth, timeGridWeek, listWeek' 
-      }
+      },
+      dateClick: function (info){
+        //console.log(info)
+        document.getElementById('start').value = info.dateStr;
+        document.getElementById('titulo').textContent = 'Registro de Salas';
+        myModal.show();
+          }
     });
     calendar.render();
   });
