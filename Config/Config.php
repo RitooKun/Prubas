483e11ffb8ef7a24f46410d98f8564845431c8f0
Kevin Andres Saldaña Acosta <kevin.saldaa@IMEVI.CO>
2024-05-28 09:24:25 -0500

    primero

---
+++ Config/Config.php
@@ -0,0 +1,8 @@
+<?php
+const base_url = "http://localhost/eventos/";
+const host = "localhost";
+const user = "root";
+const pass = "";
+const db = "sala_cirugias";
+const charset = "charset=utf8";
+?>
